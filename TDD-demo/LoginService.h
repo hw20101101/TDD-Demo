3966c2b46427186f9191032674b04076f67f6e53
huangwei <hwacdx@163.com>
2025-07-21 15:03:59 +0800

    第二步：创建最小的实现让测试通过（GREEN）

---
+++ TDD-demo/LoginService.h
@@ -0,0 +1,16 @@
+//
+//  LoginService.h
+//  TDD-demo
+//
+//  Created by 123 on 2025/7/21.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LoginService : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TDD-demo/LoginService.m
@@ -0,0 +1,12 @@
+//
+//  LoginService.m
+//  TDD-demo
+//
+//  Created by 123 on 2025/7/21.
+//
+
+#import "LoginService.h"
+
+@implementation LoginService
+
+@end
